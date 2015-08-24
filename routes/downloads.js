2980fecdc480a09b322aefca771cb67b2ca92f93
vcapicotto-ext@delfingen.com <vcapicotto-ext@delfingen.com>
2015-08-24 11:45:50 +0200

    add commentaries

--- routes/downloads.js
+++ routes/downloads.js
@@ -1,106 +1,125 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * get the list of download status
+ */
 router.get('/status',
-    function (req, res, next) {
-        var db = req.db;
-        db.query('select id, name from download_status order by ord',
-            function (err, rows) {
-                db.end();
-                if (!err)
-                    res.json(rows);
-                else
-                    console.log('Error while performing Query.');
-            }
-        );
-    }
+  function (req, res, next) {
+    var db = req.db;
+    db.query('select id, name from download_status order by ord',
+      function (err, rows) {
+        if (!err)
+          res.json(rows);
+        else
+          console.log('Error while performing Query.');
+      }
+    );
+  }
 );
 
-/* GET downloads listing. */
+/**
+ * get the list of downloads
+ */
 router.get('/',
-    function (req, res, next) {
-        var db = req.db;
-        db.query('select * from download order by id',
-            function (err, rows) {
-                db.end();
-                if (!err)
-                    res.json(rows);
-                else
-                    console.log('Error while performing Query.');
-            }
-        );
-    }
+  function (req, res, next) {
+    var db = req.db;
+    db.query('select * from download order by id',
+      function (err, rows) {
+        if (!err)
+          res.json(rows);
+        else
+          console.log('Error while performing Query.');
+      }
+    );
+  }
 );
 
+/**
+ * get a download by id
+ */
 router.get('/:id',
-    function (req, res, next) {
-        var db = req.db;
-        var id = req.params.id;
-        db.query('select * from download where id=?', [id],
-            function (err, rows) {
-                db.end();
-                if (!err)
-                    res.json(rows);
-                else
-                    console.log('Error while performing Query.');
-            }
-        );
-    }
+  function (req, res, next) {
+    var db = req.db;
+    var id = req.params.id;
+    db.query('select * from download where id=?', [id],
+      function (err, rows) {
+        if (!err)
+          res.json(rows);
+        else
+          console.log('Error while performing Query.');
+      }
+    );
+  }
 );
 
+/**
+ * add a new download
+ */
 router.post('/',
-    function (req, res) {
-        var db = req.db;
-        var down = JSON.parse(JSON.stringify(req.body));
+  function (req, res) {
+    var db = req.db;
+    var down = JSON.parse(JSON.stringify(req.body));
 
-        db.query('insert into download set ?', down,
-            function(err, rows) {
-                if (err) {
-                    res.send(err);
-                } else {
-                    down.id = rows.insertId;
-                    res.json(down);
-                }
-            }
-        )
-    }
+    db.query('insert into download set ?', down,
+      function (err, rows) {
+        if (err) {
+          res.send(err);
+        } else {
+          down.id = rows.insertId;
+          res.json(down);
+        }
+      }
+    );
+  }
 );
 
+/**
+ * update a download by id
+ */
 router.put('/:id',
-    function(req, res) {
-        var db = req.db;
-        var id = req.params.id;
-        var down = JSON.parse(JSON.stringify(req.body));
+  function (req, res) {
+    var db = req.db;
+    var id = req.params.id;
+    var down = JSON.parse(JSON.stringify(req.body));
 
-        db.query('update download set ? where id = ?', [down, id],
-            function(err, rows) {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.json(down);
-                }
-            }
-        );
-    }
+    db.query('update download set ? where id = ?', [down, id],
+      function (err, rows) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(down);
+        }
+      }
+    );
+  }
 );
 
+/**
+ * delete a download by id
+ */
 router.delete('/:id',
-    function(req, res) {
-        var db = req.db;
-        var id = req.params.id;
+  function (req, res) {
+    var db = req.db;
+    var id = req.params.id;
 
-        db.query('delete from download where id = ?', id,
-            function(err) {
-                if (err) {
-                    status = false;
-                } else {
-                    status = true;
-                }
+    db.query('delete from download where id = ?', id,
+      function (err) {
+        var status = (err === null);
 
-                res.json(status);
-            }
-        );
-    }
+        res.json(status);
+      }
+    );
+  }
+);
+
+/**
+ * refresh the list of downloads
+ */
+router.get('/refreshDownloads',
+  function (req, res) {
+
+  }
 );
 
 module.exports = router;
