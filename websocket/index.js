e435044c26822ddd48833d6e0920afd427fd5598
capic <vcapic@gmail.com>
2015-08-31 20:31:18 +0200

    websocket

--- websocket/index.js
+++ websocket/index.js
@@ -1,15 +1,17 @@
 /**
  * Created by Vincent on 31/08/2015.
  */
+var autobahn = require('autobahn');
+
 var websocket = new  autobahn.Connection({
    url: 'ws://capic.hd.free.fr:8181/ws',
     realm: 'realm1'
 });
 
-connection.onopen = function(session) {
+websocket.onopen = function(session) {
     console.log('onopen');
 };
 
-connection.open();
+websocket.open();
 
-module.export = connection;
+module.export = websocket;
