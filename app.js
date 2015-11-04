f8ed8b632b53f8418a334399120cc23b58bf0c7c
capic <vcapic@gmail.com>
2015-11-04 15:23:06 +0100

    manage error

--- app.js
+++ app.js
@@ -50,7 +50,11 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        /*res.render('error', {
+            message: err.message,
+            error: err
+        });*/
+        res.json({
             message: err.message,
             error: err
         });
@@ -61,7 +65,11 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    /*res.render('error', {
+        message: err.message,
+        error: {}
+    });*/
+    res.json({
         message: err.message,
         error: {}
     });
