ef97f025b3cccff29c89b9ef2e1edd58fbc4006c
capic <vcapic@gmail.com>
2015-11-26 12:13:25 +0100

    test debuggage

--- common/utils.js
+++ common/utils.js
@@ -11,41 +11,35 @@ var utils = {};
 
 var move = function (oldDirectory, newDirectory, name, logs, downloadModel, downloadLogsModel, downloadDirectoryModel, callback) {
     var command = 'ssh root@' + downloadServerConfig.address + ' mv ' + oldDirectory + name + ' ' + newDirectory;
-    var execMoveFile = exec(command);
-
-    // pas d'erreur de deplacement
-    execMoveFile.stdout.on('data',
-        function (data) {
-            var param = {
-                directory_id: downloadDirectoryModel.id,
-                download_directory: downloadDirectoryModel,
-                old_download_directory: downloadMode.directory_id,
-                status: downloadStatusConfig.MOVED
-            };
-            logs += "Moving from " + oldDirectory + " to " + newDirectory + " OK !!!\r\n";
-            logs += data + "\r\n";
-            callback(downloadModel, downloadLogsModel, downloadDirectoryModel, param, logs);
-        }
-    );
-
-    execMoveFile.stderr.on('data',
-        function (data) {
-            // meme si tout s'est bien passe on peut avoir cette erreur donc on considere que c'est ok
-            if (data == "ln: failed to create symbolic link `/dev/fd/fd': No such file or directory\n") {
+    var execMoveFile = exec(command,
+        function(error, stdout, stderr) {
+            if (error == null) {
                 var param = {
                     directory_id: downloadDirectoryModel.id,
                     download_directory: downloadDirectoryModel,
+                    old_download_directory: downloadMode.directory_id,
                     status: downloadStatusConfig.MOVED
                 };
                 logs += "Moving from " + oldDirectory + " to " + newDirectory + " OK !!!\r\n";
             } else {
-                var param = {
-                    status: downloadStatusConfig.ERROR_MOVING,
-                    directory_id: downloadModel.old_directory_id,
-                    old_directory_id: downloadModel.directory_id
-                };
-                logs += "Moving from " + oldDirectory + " to " + newDirectory + " ERROR !!!\r\n";
+                // meme si tout s'est bien passe on peut avoir cette erreur donc on considere que c'est ok
+                if (stdout == "ln: failed to create symbolic link `/dev/fd/fd': No such file or directory\n") {
+                    var param = {
+                        directory_id: downloadDirectoryModel.id,
+                        download_directory: downloadDirectoryModel,
+                        status: downloadStatusConfig.MOVED
+                    };
+                    logs += "Moving from " + oldDirectory + " to " + newDirectory + " OK !!!\r\n";
+                } else {
+                    var param = {
+                        status: downloadStatusConfig.ERROR_MOVING,
+                        directory_id: downloadModel.old_directory_id,
+                        old_directory_id: downloadModel.directory_id
+                    };
+                    logs += "Moving from " + oldDirectory + " to " + newDirectory + " ERROR !!!\r\n";
+                }
             }
+
             logs += data + "\r\n";
             callback(downloadModel, downloadLogsModel, downloadDirectoryModel, param, logs);
         }
