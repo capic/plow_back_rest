f38525472500d9933156df7fd584c665409f61ef
capic <vcapic@gmail.com>
2015-11-27 20:15:09 +0100

    mise a jour du pourcentage d'extraction

--- routes/downloadDirectories.js
+++ routes/downloadDirectories.js
@@ -9,80 +9,99 @@ var errorConfig = config.get('errors');
  * get the list of download directories
  */
 router.get('/',
-  function (req, res, next) {
-    var callback = function (downloadDirectories) {
-      res.json(downloadDirectories);
-    };
+    function (req, res, next) {
+        var callback = function (downloadDirectories) {
+            res.json(downloadDirectories);
+        };
 
-    var params = {};
-    for (prop in req.query) {
-      params[prop] = req.query[prop];
-    }
+        var params = {};
+        for (prop in req.query) {
+            params[prop] = req.query[prop];
+        }
 
-    if (Object.keys(params).length !== 0) {
-      models.DownloadDirectory.findAll({
-        where: params
-      }).then(callback);
-    } else {
-      models.DownloadDirectory.findAll().then(callback);
+        if (Object.keys(params).length !== 0) {
+            models.DownloadDirectory.findAll({
+                where: params
+            }).then(callback);
+        } else {
+            models.DownloadDirectory.findAll().then(callback);
+        }
     }
-  }
 );
 
 /**
  * get a download by id
  */
 router.get('/:id',
-  function (req, res, next) {
-    models.DownloadDirectory.findById(req.params.id)
-      .then(function (downloadDirectoryModel) {
-        res.json(downloadDirectoryModel);
-      }
-    );
-  }
+    function (req, res, next) {
+        models.DownloadDirectory.findById(req.params.id)
+            .then(function (downloadDirectoryModel) {
+                res.json(downloadDirectoryModel);
+            }
+        );
+    }
 );
 
 /**
  * add a new download
  */
 router.post('/',
-  function (req, res) {
-    var downloadDirectoryObject = JSON.parse(JSON.stringify(req.body));
+    function (req, res) {
+        var downloadDirectoryObject = JSON.parse(JSON.stringify(req.body));
 
-    models.DownloadDirectory.findOrCreate({where: {path: downloadDirectoryObject.path}, defaults: downloadDirectoryObject})
-      .spread(function (downloadDirectoryModel, created) {
-        res.json(downloadDirectoryModel.get({plain: true}));
-      }
-    );
-  }
+        models.DownloadDirectory.findOrCreate({
+            where: {path: downloadDirectoryObject.path},
+            defaults: downloadDirectoryObject
+        })
+            .spread(function (downloadDirectoryModel, created) {
+                res.json(downloadDirectoryModel.get({plain: true}));
+            }
+        );
+    }
+);
+
+router.put('/:id/unrarPercent',
+    function (req, res) {
+        var downloadDirectoryObject = JSON.parse(JSON.stringify(req.body));
+
+        models.DownloadDirectory.findById(req.params.id)
+            .then(function (downloadDirectoryModel) {
+                downloadDirectoryModel.updateAttributes({unrar_percent: downloadDirectoryObject.percent})
+                    .then(function () {
+                        res.json(downloadDirectoryModel);
+                    }
+                );
+            }
+        );
+    }
 );
 
 /**
  * delete a download by id
  */
 router.delete('/:id',
-  function (req, res, next) {
-    models.Download.findAndCountAll({
-        where: {
-          directory_id: req.params.id,
-          $or: [{status: downloadStatusConfig.WAITING}, {status: downloadStatusConfig.IN_PROGRESS}]
-        }
-    })
-      .then(function(result) {
-        // on supprime le directory seulement si il n'est pas utilise autre part
-        if (result.count <= 1) {
-          models.DownloadDirectory.destroy({where: {id: req.params.id}})
-            .then(function (ret) {
-              res.json({'return': ret == 1});
+    function (req, res, next) {
+        models.Download.findAndCountAll({
+            where: {
+                directory_id: req.params.id,
+                $or: [{status: downloadStatusConfig.WAITING}, {status: downloadStatusConfig.IN_PROGRESS}]
             }
-          );
-        } else {
-          var error = new Error(res.__(errorConfig.downloadDirectories.deleteDirectory.message));
-          error.status = errorConfig.downloadDirectories.deleteDirectory.code;
-          return next(error);
-        }
-      });
-  }
+        })
+            .then(function (result) {
+                // on supprime le directory seulement si il n'est pas utilise autre part
+                if (result.count <= 1) {
+                    models.DownloadDirectory.destroy({where: {id: req.params.id}})
+                        .then(function (ret) {
+                            res.json({'return': ret == 1});
+                        }
+                    );
+                } else {
+                    var error = new Error(res.__(errorConfig.downloadDirectories.deleteDirectory.message));
+                    error.status = errorConfig.downloadDirectories.deleteDirectory.code;
+                    return next(error);
+                }
+            });
+    }
 );
 
 module.exports = router;
