1c65190461101cda9f751f9d3794d916d5b970ac
capic <vcapic@gmail.com>
2015-12-03 22:31:09 +0100

    unrar

--- routes/downloads.js
+++ routes/downloads.js
@@ -486,8 +486,8 @@ router.post('/unrar',
 
         var command = 'ssh root@' + downloadServerConfig.address + ' ' + downloadServerConfig.unrar_command + ' ' + dataObject.id;
         var s = spawn('ssh', ['root@' + downloadServerConfig.address, downloadServerConfig.unrar_command, dataObject.id]);
-        s.stdout.on('data', function (data) { console.log(data) });
-        s.stderr.on('data', function (data) { console.log(data) });
+        s.stdout.on('data', function (data) { console.log(data.toString()) });
+        s.stderr.on('data', function (data) { console.log(data.toString()) });
         s.on('exit', function(code) {
             if (code != 0) {
                 console.log('Failed: ' + code);
