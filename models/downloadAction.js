d28a35e2dd180da739e81483c44d436939c320d2
capic <vcapic@gmail.com>
2015-12-07 16:30:57 +0100

    init download action

--- models/downloadAction.js
+++ models/downloadAction.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
     tableName: 'download_action',
     classMethods: {
       associate: function(models) {
-        DownloadAction.hasMany(models.download)
+        DownloadAction.hasMany(models.Download)
       }
     }
   });
