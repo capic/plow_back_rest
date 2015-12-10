7f97fc11a032426a6ed8c36045f2fd1f629dd443
capic <vcapic@gmail.com>
2015-12-10 18:07:35 +0100

    init download action

--- models/downloadActionComposedByProperties.js
+++ models/downloadActionComposedByProperties.js
@@ -8,7 +8,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false,
             primaryKey: true
         },
-        download_action_property: {
+        download_action_property_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true
