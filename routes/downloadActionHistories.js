529e0aecc41cf4d71ff32ccbf01e9535edf58efd
capic <vcapic@gmail.com>
2015-12-11 08:15:22 +0100

    init download action

--- routes/downloadActionHistories.js
+++ routes/downloadActionHistories.js
@@ -26,7 +26,14 @@ router.get('/',
                     {
                         model: models.DownloadAction, as: 'download_action',
                         include: [
-                            {model: models.DownloadActionComposedByProperties, as: 'download_action_composed_by_properties'}
+                            {
+                                model: models.DownloadActionComposedByProperties, as: 'download_action_composed_by_properties',
+                                include: [
+                                    {
+                                        model: models.DownloadActionProperty, as: 'download_action_property'
+                                    }
+                                ]
+                            }
                         ]
                     },
                     {model: models.DownloadActionStatus, as: 'download_action_status'}
