d739e03a19ffe5eeb0dccbf051bad9b808e82730
capic <vcapic@gmail.com>
2015-12-28 02:12:30 +0100

    modification du schema de bdd

--- models/action.js
+++ models/action.js
@@ -1,33 +1,33 @@
 /**
  * Created by Vincent on 27/08/2015.
  */
-module.exports = function(sequelize, DataTypes) {
-  var Action = sequelize.define('Action', {
-    lifecycle_insert_date: DataTypes.DATE,
-    lifecycle_update_date: DataTypes.DATE
-  }, {
-    freezeTableName: true,
-    createdAt: false,
-    updatedAt: false,
-    tableName: 'action',
-    classMethods: {
-      associate: function(models) {
-        Action.belongsTo(models.Download, {
-          foreignKey: 'download_id',
-          as: 'download'
-        });
-        Action.belongsTo(models.ActionStatus, {
-          foreignKey: 'action_status_id',
-          as: 'action_status'
-        });
-        Action.belongsTo(models.ActionType, {
-          foreignKey: 'action_type_id',
-          as: 'action_type'
-        });
-        Action.hasMany(models.ActionHasProperties);
-      }
-    }
-  });
+module.exports = function (sequelize, DataTypes) {
+    var Action = sequelize.define('Action', {
+        lifecycle_insert_date: DataTypes.DATE,
+        lifecycle_update_date: DataTypes.DATE
+    }, {
+        freezeTableName: true,
+        createdAt: false,
+        updatedAt: false,
+        tableName: 'action',
+        classMethods: {
+            associate: function (models) {
+                Action.belongsTo(models.Download, {
+                    foreignKey: 'download_id',
+                    as: 'download'
+                });
+                Action.belongsTo(models.ActionStatus, {
+                    foreignKey: 'action_status_id',
+                    as: 'action_status'
+                });
+                Action.belongsTo(models.ActionType, {
+                    foreignKey: 'action_type_id',
+                    as: 'action_type'
+                });
+                Action.hasMany(models.ActionHasProperties, {foreignKey: 'action_id', as: 'action_has_properties'});
+            }
+        }
+    });
 
-  return Action;
+    return Action;
 };
