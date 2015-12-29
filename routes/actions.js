b87a3d76f2836a3de1693d8450a57239cc17e774
capic <vcapic@gmail.com>
2015-12-29 17:23:28 +0100

    creation d'une methode execute pour les actions

--- routes/actions.js
+++ routes/actions.js
@@ -148,6 +148,7 @@ router.post('/execute',
         var actionToExecute = JSON.parse(JSON.stringify(req.body));
 
         var command = 'ssh root@' + downloadServerConfig.address + ' ' + downloadServerConfig.action_command + ' ' + actionToExecute.download_id + ' ' + actionToExecute.action_id;
+        console.log(command);
         var execAction = exec(command);
         execAction.stdout.on('data',
             function(data) {
