7fb6229bd1f8b3a75de66a1a4b59adb2eda9c778
capic <vcapic@gmail.com>
2016-08-03 11:22:14 +0200

    Update downloads.js

--- routes/downloads.js
+++ routes/downloads.js
@@ -18,7 +18,6 @@ router.get('/status',
     function (req, res, next) {
         models.DownloadStatus.findAll()
             .then(function (downloadStatusModel) {
-                    res.header("Content-Type", "application/json; charset=utf-8");
                     res.json(downloadStatusModel);
                 }
             );
